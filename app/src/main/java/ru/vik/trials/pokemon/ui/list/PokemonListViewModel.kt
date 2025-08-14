ab51aa4969f1454dadc1d4e3fa8c7238072f05e0
vikvlg <linkvlg@yandex.ru>
2025-08-14 14:47:33 +0300

    Тестовая фильтрация списка покемонов.

--- app/src/main/java/ru/vik/trials/pokemon/ui/list/PokemonListViewModel.kt
+++ app/src/main/java/ru/vik/trials/pokemon/ui/list/PokemonListViewModel.kt
@@ -7,8 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.filter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import ru.vik.trials.pokemon.domain.GetPokemonListUseCase
 import ru.vik.trials.pokemon.domain.entities.Pokemon
@@ -31,12 +33,31 @@ class PokemonListViewModel @Inject constructor(
 
     /** Получает список покемонов. */
     fun refresh() {
-        Log.d(TAG, "refresh...")
+        Log.d("TAG", "[${Thread.currentThread().name}] refresh...")
         viewModelScope.launch(Dispatchers.IO) {
-            getPokemonListUseCase().cachedIn(viewModelScope).collect {
-                Log.d("TAG", "[${Thread.currentThread().name}] CharacterListViewModel refresh")
+            Log.d("TAG", "[${Thread.currentThread().name}] getPokemonListUseCase")
+
+            // TODO: Из-за cachedIn код сценария выполняется в main-потоке,
+            // было бы неплохо с этим разобраться.
+            getPokemonListUseCase().cachedIn(viewModelScope).map { pagingData ->
+                pagingData.filter {
+                    //true
+                    //(it.base.id % 10) == 9
+                    val types = it.details?.types ?: ""
+                    val isAvailable = (types.indexOf("poison") != -1)
+                    if (!isAvailable) {
+                        Log.d("TAG", "ignore #${it.base.id} ${it.base.name} = ${it.details?.types}")
+                    }
+                    isAvailable
+                }
+            }.collect {
                 pokemonList.postValue(it)
             }
+
+//            getPokemonListUseCase().cachedIn(viewModelScope).collect {
+//                Log.d("TAG", "[${Thread.currentThread().name}] getPokemonListUseCase.collect")
+//                pokemonList.postValue(it)
+//            }
         }
     }
 }
